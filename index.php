b462d6ee29237364540de9b142e9a0a625069d57
Pratik Parija <pp285@afsconnect1.njit.edu>
2017-09-19 00:06:28 -0400

    Add index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  
+  $class = 'is601';
+  $message = 'Hello';
+  echo "Hello $class </br>";
+  echo 'Hello I\'m ' .  $class . ' continue the sentence</br>';
+
+  $message .= $class;
+  
+  $a = 1;
+  $b = 2;
+
+  $c = $a + $b;
+
+  echo $c;
+
+  echo $message;
+
+?>
